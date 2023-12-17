94dce9c5c5bc4b676c4a941af9e5d51aca40105f
IshaanVijayPuniya <85081353+IshaanVijayPuniya@users.noreply.github.com>
2023-12-17 16:13:02 +0100

    Add files via upload

---
+++ maui/Platforms/Android/MainApplication.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Runtime;
+
+// Needed for Picking photo/video when selecting the Image for a sneaker
+[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage)]
+
+// Needed for Taking photo/video if you want to take a picture of the shoe 
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+[assembly: UsesPermission(Android.Manifest.Permission.Camera)]
+
+// Add these properties if you would like to filter out devices that do not have cameras, or set to false to make them optional
+[assembly: UsesFeature("android.hardware.camera", Required = true)]
+[assembly: UsesFeature("android.hardware.camera.autofocus", Required = true)]
+
+namespace maui;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
+	{
+	}
+
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
